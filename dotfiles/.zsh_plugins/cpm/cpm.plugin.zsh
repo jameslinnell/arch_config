9d00707e521529477a4c66c1140f9f8429fef610
jameslinnell <jimlinnell@gmail.com>
2024-12-06 20:15:20 +0000

    Some dotfiles

---
+++ dotfiles/.zsh_plugins/cpm/cpm.plugin.zsh
@@ -0,0 +1,17 @@
+# cpm.plugin.zsh
+#
+# Add custom aliases for cpm
+#
+alias cpm="cdcpm"
+alias cpmb="cpm-build"
+alias cpmf="cpm-full"
+alias cpms="cpm-stop"
+alias cpmt="cpm-test"
+alias cpmut="cpm-test-unit"
+alias cpmftl="cpm-test-floc"
+alias cpmfti="cpm-test-fint"
+alias cpmit="cpm-test-int"
+alias cpmst="cpm-test-smoke"
+
+
+
